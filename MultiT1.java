7bbaa6a9fc8cbac2f8aeb29296b442e96eeb20e6
wisher18 <risingyouth005@gmail.com>
2023-02-23 18:58:24 +0530

    Initial Commit

--- MultiT1.java
+++ MultiT1.java
@@ -1,16 +1,27 @@
-class Process1{
-    public void fun1(){
-        
+class Process1 implements Runnable{
+    public void run(){
+        int i;
+        for(i =1; i <=10; i ++)
+        System.out.println("P1: "+i);
     }
 }
-class Process2{
-    public void fun2(){
+class Process2 implements Runnable{
+    public void run(){
+        int i;
+        for(i=1; i <=10; i++)
+        System.out.println("P2: "+i);
 
     }
 }
 public class MultiT1 {
     public static void main(String[] args) {
-        
+        Process1 p1 = new Process1();
+        Process2 p2 = new Process2();
+
+        Thread t1 = new Thread(p1);
+        Thread t2 = new Thread(p2);
+        t1.start();
+        t2.start();
         
     }
     
