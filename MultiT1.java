6a7d16142a05bfa5113f04a0f8f44108dbe04df7
wisher18 <risingyouth005@gmail.com>
2023-02-23 17:22:34 +0530

    Second commit

---
+++ MultiT1.java
@@ -0,0 +1,18 @@
+class Process1{
+    public void fun1(){
+        
+    }
+}
+class Process2{
+    public void fun2(){
+
+    }
+}
+public class MultiT1 {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
+
--- Scan.java
+++ Scan.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.swing.event.SwingPropertyChangeSupport;
-
 public class Scan {
     public static void main(String[] args) {
         
